415186df98c8eac87229fe8be354c3e3b9bbb173
GiovanniEncisoDev <giovanni_enciso.s@hotmail.com>
2025-03-13 07:53:06 -0500

    Correcciones

---
+++ Layout/dist/script.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Acceder al botón y agregar un evento de clic
+document.getElementById('changeContentButton').addEventListener('click', function () {
+  // Cambiar el texto del párrafo
+  document.getElementById('infoText').innerHTML = 'El término bloatware se refiere a software que consume recursos excesivos, incluso para tareas simples.'; // Cambiar la imagen al hacer clic
+
+  document.getElementById('bloatwareImage').src = '/Layout/Que es.jpg'; // Cambia esto por la ruta de otra imagen
+});
