a5853d5af35e5f5b0c4a9ee4eb255a0cc236580f
GiovanniEncisoDev <giovanni_enciso.s@hotmail.com>
2025-04-02 07:31:50 -0500

    common/ecma

---
+++ NuevoMundoEcma/dist/index.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _cowsay = _interopRequireDefault(require("cowsay"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+//const cowsay = require("cowsay");
+console.log(_cowsay["default"].say({
+  text: "I'm a moooodule",
+  e: "oO",
+  T: "U "
+})); //console.log("Hola mundo");
